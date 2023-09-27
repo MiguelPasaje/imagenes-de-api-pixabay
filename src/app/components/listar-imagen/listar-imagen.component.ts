345b5e98d663e07ecd30957fe470b0b2a424a414
MiguelPasaje <cartel-dy@hotmail.com>
2023-09-26 23:05:26 -0500

    mostrar imagenes de la api

--- src/app/components/listar-imagen/listar-imagen.component.ts
+++ src/app/components/listar-imagen/listar-imagen.component.ts
@@ -10,11 +10,14 @@ import { Subscription } from 'rxjs';
 export class ListarImagenComponent {
   termino = '';
   subscription: Subscription;
+  listImagenes: any[] = [];
+  loading:boolean = false
 
   constructor(private _imagenService: ImagenService) {
     this.subscription = this._imagenService
-      .getTerminoBusqueda()
-      .subscribe((data) => {
+    .getTerminoBusqueda()
+    .subscribe((data) => {
+        this.loading = true
         //console.log(data, 'a');
         this.termino = data;
         this.obtenerImagenes();
@@ -25,15 +28,18 @@ export class ListarImagenComponent {
     this._imagenService.getImagenes(this.termino).subscribe(
       (data) => {
         console.log(data);
+        this.loading = false
         if (data.hits.length == 0) {
           this._imagenService.setError(
             `No se encontro ningun resultado para ${this.termino}`
           );
           return;
         }
+        this.listImagenes = data.hits;
       },
       (error) => {
         this._imagenService.setError(`Upss.. ocurrio un error.`);
+        this.loading = false
       }
     );
   }
